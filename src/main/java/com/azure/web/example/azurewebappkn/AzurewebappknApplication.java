a1368ce58ff29a01935dd27ebffe930337cbbef8
Ketan Nakum <ketan.nakum@gmail.com>
2018-11-27 22:52:13 -0500

    Initial Commit to Master

---
+++ src/main/java/com/azure/web/example/azurewebappkn/AzurewebappknApplication.java
@@ -0,0 +1,12 @@
+package com.azure.web.example.azurewebappkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurewebappknApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurewebappknApplication.class, args);
+	}
+}
